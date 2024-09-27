616abeab9e6d6cb4516e1714d3e35042b80894e3
Arman Yerkesh <a.yerkesh@gmail.com>
2024-09-27 14:44:40 +0300

    Scaffold parliament application

--- BluetoothLE/app/src/main/java/com/example/bluetoothle/MainViewModel.kt
+++ BluetoothLE/app/src/main/java/com/example/bluetoothle/MainViewModel.kt
@@ -2,8 +2,10 @@ package com.example.bluetoothle
 
 import android.Manifest
 import android.annotation.SuppressLint
+import android.bluetooth.BluetoothAdapter
 import android.bluetooth.le.BluetoothLeScanner
 import android.bluetooth.le.ScanCallback
+import android.bluetooth.le.ScanFilter
 import android.bluetooth.le.ScanResult
 import android.bluetooth.le.ScanSettings
 import android.content.pm.PackageManager
@@ -21,50 +23,50 @@ import kotlinx.coroutines.launch
 import java.util.UUID
 
 class MainViewModel : ViewModel() {
-    val scanResults = MutableLiveData<List<ScanResult>>(null)
-    val fScanning = MutableLiveData<Boolean>(false)
-    private val mResults = java.util.HashMap<String, ScanResult>()
-
-    companion object GattAttributes {
-        const val SCAN_PERIOD: Long = 5000
-        const val STATE_DISCONNECTED = 0
-        const val STATE_CONNECTING = 1
-        const val STATE_CONNECTED = 2
-        val UUID_HEART_RATE_MEASUREMENT = UUID.fromString("00002a37-0000-1000-8000-00805f9b34fb")
-        val UUID_HEART_RATE_SERVICE = UUID.fromString("0000180d-0000-1000-8000-00805f9b34fb")
-        val UUID_CLIENT_CHARACTERISTIC_CONFIG = UUID.fromString("00002902-0000-1000-8000-00805f9b34fb")
-    }
-
-    fun scanDevices(scanner: BluetoothLeScanner) {
-        viewModelScope.launch(Dispatchers.IO) {
-            fScanning.postValue(true)
-            val settings = ScanSettings.Builder()
-                .setScanMode(ScanSettings.SCAN_MODE_LOW_LATENCY)
-                .setReportDelay(0)
-                .build()
-            scanner.startScan(null, settings, leScanCallback)
-            delay(SCAN_PERIOD)
-            scanner.stopScan(leScanCallback)
-            Log.d("DBG", mResults.toString())
-            scanResults.postValue(mResults.values.toList())
-            fScanning.postValue(false)
-        }
-    }
-
-    private val leScanCallback: ScanCallback = object : ScanCallback() {
-        override fun onScanResult(callbackType: Int, result: ScanResult) {
-            Log.d("DBG", "onScanResult")
-            super.onScanResult(callbackType, result)
-            val device = result.device
-            val deviceAddress = device.address
-            mResults[deviceAddress] = result
-            Log.d("DBG", "Device address: $deviceAddress (${result.toString()})")
-            scanResults.postValue(mResults.values.toList())
-        }
-
-        override fun onScanFailed(errorCode: Int) {
-            Log.d("DBG", "onScanFailed: $errorCode")
-            super.onScanFailed(errorCode)
-        }
-    }
+//    val scanResults = MutableLiveData<List<ScanResult>>(null)
+//    val fScanning = MutableLiveData<Boolean>(false)
+//    private val mResults = java.util.HashMap<String, ScanResult>()
+//
+//    companion object GattAttributes {
+//        const val SCAN_PERIOD: Long = 5000
+//        const val STATE_DISCONNECTED = 0
+//        const val STATE_CONNECTING = 1
+//        const val STATE_CONNECTED = 2
+//        val UUID_HEART_RATE_MEASUREMENT = UUID.fromString("00002a37-0000-1000-8000-00805f9b34fb")
+//        val UUID_HEART_RATE_SERVICE = UUID.fromString("0000180d-0000-1000-8000-00805f9b34fb")
+//        val UUID_CLIENT_CHARACTERISTIC_CONFIG = UUID.fromString("00002902-0000-1000-8000-00805f9b34fb")
+//    }
+//
+//    fun scanDevices(scanner: BluetoothLeScanner) {
+//        viewModelScope.launch(Dispatchers.IO) {
+//            fScanning.postValue(true)
+//            val settings = ScanSettings.Builder()
+//                .setScanMode(ScanSettings.SCAN_MODE_LOW_LATENCY)
+//                .setReportDelay(0)
+//                .build()
+//            scanner.startScan(null, settings, leScanCallback)
+//            delay(SCAN_PERIOD)
+//            scanner.stopScan(leScanCallback)
+//            Log.d("DBG", mResults.toString())
+//            scanResults.postValue(mResults.values.toList())
+//            fScanning.postValue(false)
+//        }
+//    }
+//
+//    private val leScanCallback: ScanCallback = object : ScanCallback() {
+//        override fun onScanResult(callbackType: Int, result: ScanResult) {
+//            Log.d("DBG", "onScanResult")
+//            super.onScanResult(callbackType, result)
+//            val device = result.device
+//            val deviceAddress = device.address
+//            mResults[deviceAddress] = result
+//            Log.d("DBG", "Device address: $deviceAddress (${result.toString()})")
+//            scanResults.postValue(mResults.values.toList())
+//        }
+//
+//        override fun onScanFailed(errorCode: Int) {
+//            Log.d("DBG", "onScanFailed: $errorCode")
+//            super.onScanFailed(errorCode)
+//        }
+//    }
 }
---
+++ Gridexample/app/src/main/java/com/example/gridexample/DataSource.kt
@@ -0,0 +1,4 @@
+package com.example.gridexample
+
+class DataSource {
+}
